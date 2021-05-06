073a52bcdeaa4de4c82d42ce2469808b4db6b126
fatimaverynice <fatimaverynice@gmail.com>
2021-05-06 12:23:45 -0500

    Cambio de , a .
    No dejaba con coma después de cv2 así que era necesario eso

--- prueba_video.py
+++ prueba_video.py
@@ -7,7 +7,7 @@ while cap.isOpened():
     #BGR image feed from camera
     ret, img = cap.read()
     #BGR to grayscale
-    img_gray = cv2.cvtColor(img,cv2,COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     cv2.GaussianBlur(img_gray,(21,21), 0, img_gray)
     edges = cv2.Canny(img_gray, 10, 20, apertureSize = 3)
 
